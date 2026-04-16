7bb6aa37f4e01ad688afe4446a940e7290f52757
swissymissy <pnghi1606@gmail.com>
2026-04-15 21:33:00 -0400

    add: create jade database, and products table

--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
---
+++ sql/schema/001_products.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE products (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        TEXT NOT NULL,
+    slug        TEXT NOT NULL UNIQUE,
+    type        TEXT NOT NULL,
+    price       REAL NOT NULL CHECK(price>0),
+    quantity    INTEGER NOT NULL DEFAULT 0 CHECK(quantity>=0),
+    description TEXT,
+    is_available    INTEGER NOT NULL DEFAULT 1,
+    video_url       TEXT,
+    created_at      TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at      TEXT NOT NULL DEFAULT (datetime('now'))
+);
+
+-- +goose Down
+DROP TABLE products;
