0ebdf4a62852bc3aa665f8f01d27272d130c1c8f
swissymissy <pnghi1606@gmail.com>
2026-04-15 22:57:01 -0400

    add: wire up database connection to http server

--- main.go
+++ main.go
@@ -11,12 +11,84 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	_ "modernc.org/sqlite"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/swissymissy/jade/database"
 )
+
+type ApiConfig struct {
+	port      string
+	platform  string
+	db        *database.Queries
+	jwtSecret string
+	s3Bucket  string
+	s3Region  string
+	s3Client  *s3.Client
+}
 
 func main() {
 
 	godotenv.Load()
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM environment variable is not set")
+	}
+
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	dbURL := os.Getenv("DB_PATH")
+	if dbURL == "" {
+		log.Fatal("DB_URL should be set")
+	}
+	// connect to database
+	db, err := sql.Open("sqlite", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database", err)
+	}
+	defer db.Close()
+	// ping to verify db connection
+	if err := db.Ping(); err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+	log.Println("Connected to database")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET is not set")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	if s3Bucket == "" {
+		log.Fatal("S3_BUCKET environment variable is not set")
+	}
+
+	s3Region := os.Getenv("S3_REGION")
+	if s3Region == "" {
+		log.Fatal("S3_REGION environment variable is not set")
+	}
+
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatal("Failed to load AWS config: %v", err)
+	}
+
+	s3NewClient := s3.NewFromConfig(awsCfg)
+
+	// initialize apiconfig
+	apicfg := ApiConfig{
+		port: port,
+		platform: platform,
+		db: db,
+		jwtSecret: jwtSecret,
+		s3Bucket: s3Bucket,
+		s3Region: s3Region,
+		s3Client: s3NewClient,
+	}
 
 	// serve mux
 	mux := http.NewServeMux()
