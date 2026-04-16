bfceca0a740bf2157de920d12e72b68f34044941
swissymissy <pnghi1606@gmail.com>
2026-04-16 17:10:11 -0400

    add: add middleware to check for auth

---
+++ internal/middleware/auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/swissymissy/jade/internal/auth"
+)
+
+// middleware to check for auth
+func AuthRequired(next http.HandleFunc, jwtSecret string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// get token from header
+		token, err := auth.GetBearerToken(r.Header)
+		if err != nil {
+			log.Printf("Missing or Invalid auth header: %s\n", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// validate token
+		adminID, err := auth.ValidateJWT(token, jwtSecret)
+		if err != nil {
+			log.Printf("Invalid token: %s\n", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// token is valid, log and let the request go though
+		log.Printf("Authenticated admin: %s\n", adminID)
+		next.ServeHTTP(w, r)
+	}
+}
