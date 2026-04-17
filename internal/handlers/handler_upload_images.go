bb517b89f578eaf1137b488c8bffd87fbfc93d05
swissymissy <pnghi1606@gmail.com>
2026-04-16 20:35:33 -0400

    add: generate random filename helper

--- internal/handlers/handler_upload_images.go
+++ internal/handlers/handler_upload_images.go
@@ -2,10 +2,17 @@ package handlers
 
 import (
 	"net/http"
+	"fmt"
 	"log"
 	"strconv"
 	"errors"
 	"database/sql"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/swissymissy/jade/internal/database"
+	"github.com/swissymissy/jade/internal/storage"
 )
 
 // handler to let admin upload images
@@ -20,8 +27,8 @@ func (apicfg *ApiConfig) HandlerUploadImages(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	// validate id
-	item, err := apicfg.DB.GetProductByID(r.Context(), itemID)
+	// check if item exists
+	_, err = apicfg.DB.GetProductByID(r.Context(), itemID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			ResponseWithError(w, http.StatusNotFound, "Item not found")
@@ -32,5 +39,66 @@ func (apicfg *ApiConfig) HandlerUploadImages(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	// check images count 
+	count, err := apicfg.DB.CountImagesByProductID(r.Context(), itemID)
+	if err != nil {
+		log.Printf("Error counting images: %s", err)
+		ResponseWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+	// check amount of imgs
+	if count >= 5 {
+		ResponseWithError(w, http.StatusBadRequest, "Maximum 5 images per product")
+		return
+	}
+
+	// read file from form
+	const maxSize = 10 << 20 //10MB
+	r.Body = http.MaxBytesReader(w, r.Body, maxSize)
+
+	// parse file multiparts
+	err = r.ParseMultipartForm(maxSize)
+	if err != nil {
+		log.Printf("Error parsing form: %s", err)
+		ResponseWithError(w, http.StatusBadRequest, "File too large or invalid form")
+		return
+	}
+
+	// retrieve the uploaded image from request
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		log.Printf("Error reading file: %s", err)
+		ResponseWithError(w, http.StatusBadRequest, "No image file provided")
+		return
+	}
+	defer file.Close() // close file to prevent data leak
+
+	// validate file type
+	extension := strings.ToLower(filepath.Ext(header.Filename))
+	allowedTypes := map[string]string{
+		".jpg":  "image/jpeg",
+		".jpeg": "image/jpeg",
+		".png":  "image/png",
+		".webp": "image/webp",
+	}
+
+	contentType, ok := allowedTypes[extension]
+	if !ok {
+		ResponseWithError(w, http.StatusBadRequest, "Only jpg, png, and webp images are allowed")
+		return
+	}
+
+	// create unique filename
+	filename, err := storage.RandomFilename(extension)
+	if err != nil {
+		log.Printf("Error generating filename: %s", err)
+		ResponseWithError(w, http.StatusInternalServerError, "Something went wrong")
+		return
+	}
+
+	// s3 key
+	s3Key := fmt.Sprintf("products/%d/%s", itemID, filename)
+
+	// upload to s3
 	
 }
---
+++ internal/storage/generate_random_filename.go
@@ -0,0 +1,19 @@
+package storage
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+// generate randome filename 
+func RandomFilename(extension string) (string, error) {
+	randomSlice := make([]byte, 32)
+	_, err := rand.Read(randomSlice)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random filename: %w", err)
+	}
+	randomName := base64.RawURLEncoding.EncodeToString(randomSlice) + extension
+	return randomName, nil
+}
+
